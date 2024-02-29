54efc03e5e2e91239d20536af320aa28fcec716a
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-02-29 10:17:35 +0100

    split into training, evaluation data

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
-from util import get_data
+from util import get_data, encode_text, decode_text
+import torch
 
 encoded_text, encoder, decoder = get_data()
 
-print(encoded_text[:10])
+data = torch.tensor(encoded_text, dtype=torch.long)
+
+n = int(0.9 * len(data))
+train_data, val_data = data[:n], data[n:]
+
+block_size = 8
+print(train_data[:block_size + 1])
