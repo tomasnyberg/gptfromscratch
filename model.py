f9ff35e47b297d2f15507aaaca9742ee07dc2e12
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-02-29 10:57:42 +0100

    add bigram language model

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
-from util import get_data, get_batch
+from util import get_data, get_batch, BigramLanguageModel
 import torch
 
 encoded_text, encoder, decoder = get_data()
+vocab_size = len(encoder)
 
 data = torch.tensor(encoded_text, dtype=torch.long)
 
@@ -12,4 +13,6 @@ batch_size = 4
 block_size = 8
 
 xb, yb = get_batch(train_data, batch_size, block_size)
-print(xb, yb)
+model = BigramLanguageModel(vocab_size)
+out = model(xb, yb)
+print(out)
--- util.py
+++ util.py
@@ -1,4 +1,7 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(420)
 
 def get_text():
     with open('input.txt', 'r') as f:
@@ -30,3 +33,13 @@ def get_batch(data, batch_size, block_size):
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     return x, y
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets):
+        logits = self.token_embedding_table(idx)
+        return logits
