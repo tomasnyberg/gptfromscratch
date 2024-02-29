fd5ba2badcafe616c53a8af364fe22637355b525
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-02-29 11:39:54 +0100

    add generate function and call it

--- model.py
+++ model.py
@@ -1,8 +1,14 @@
-from util import get_data, get_batch, BigramLanguageModel
+from util import get_data, get_batch, BigramLanguageModel, encode_text_with_encoder, decode_text_with_decoder
 import torch
 
 encoded_text, encoder, decoder = get_data()
 vocab_size = len(encoder)
+
+def encode(text):
+    return encode_text_with_encoder(text, encoder)
+
+def decode(encoded_text): 
+    return decode_text_with_decoder(encoded_text, decoder)
 
 data = torch.tensor(encoded_text, dtype=torch.long)
 
@@ -16,5 +22,5 @@ xb, yb = get_batch(train_data, batch_size, block_size)
 model = BigramLanguageModel(vocab_size)
 
 logits, loss = model(xb, yb)
-print(logits.shape)
-print(loss)
+idx = torch.zeros((1,1), dtype=torch.long)
+print(decode(model.generate(idx, max_new_tokens=100)[0].tolist()))
--- util.py
+++ util.py
@@ -15,17 +15,17 @@ def get_vocab(text):
 def encoder_decoder(vocab):
     return {c: i for i, c in enumerate(vocab)}, {i: c for i, c in enumerate(vocab)}
 
-def encode_text(text, encoder):
+def encode_text_with_encoder(text, encoder):
     return [encoder[c] for c in text]
 
-def decode_text(encoded_text, decoder):
+def decode_text_with_decoder(encoded_text, decoder):
     return ''.join([decoder[i] for i in encoded_text])
 
 def get_data():
     text = get_text()
     vocab = get_vocab(text)
     encoder, decoder = encoder_decoder(vocab)
-    encoded_text = encode_text(text, encoder)
+    encoded_text = encode_text_with_encoder(text, encoder)
     return encoded_text, encoder, decoder
 
 def get_batch(data, batch_size, block_size):
@@ -40,10 +40,22 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
-    def forward(self, idx, targets):
+    def forward(self, idx, targets=None):
         logits = self.token_embedding_table(idx)
-        B, T, C = logits.shape
-        logits = logits.view(B * T, C)
-        targets = targets.view(B*T)
-        loss = F.cross_entropy(logits, targets)
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
         return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
