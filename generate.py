a7d32856178ab342be8fbba2904f64d260a6b6ae
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-01 03:14:26 +0100

    correct bug with device in generate.py

--- generate.py
+++ generate.py
@@ -3,9 +3,10 @@ import torch
 
 epoch_to_load = "1000"
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model, optimizer, _ = load_model(None, None, epoch_to_load)
 print(f"Loaded model from checkpoint {epoch_to_load}!")
 print("Generating shakespeare impression...")
 print("--------------\n\n")
-idx = torch.zeros((1, 1), dtype=torch.long)
+idx = torch.zeros((1, 1), dtype=torch.long).to(device)
 print(decode(model.generate(idx, max_new_tokens=1000)[0].tolist()))
